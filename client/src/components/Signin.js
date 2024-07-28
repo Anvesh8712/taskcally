766a8ee21b21c9ae7d834063a968beb5fd6cc7a1
Anvesh <atvanvesh@gmail.com>
2024-07-27 21:40:13 -0500

    minor changes to frontend

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -120,15 +120,10 @@ export default function SignIn() {
               Sign In with Google
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="/signup" variant="body2">
+                <Button onClick={() => navigate("/signup")} variant="body2">
                   {"Don't have an account? Sign Up"}
-                </Link>
+                </Button>
               </Grid>
             </Grid>
           </Box>
