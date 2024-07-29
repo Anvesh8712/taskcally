d2e9379f4d00a288ae68fec37ae8ff9b701a5ded
Anvesh <atvanvesh@gmail.com>
2024-07-29 15:35:50 -0500

    backend

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.MONGODB_URI;
-const uri =
-  "mongodb+srv://atvanvesh:Shilpa123456+@taskcally.3uioyib.mongodb.net/?retryWrites=true&w=majority&appName=taskcally";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
